00da78e51d9aab9319368a90e5fd67ceeeffbad9
feng.dev <feng@vitruvio.cl>
2024-11-11 19:05:13 -0300

    ...

--- mke/target/debug/deps/mke.d
+++
@@ -1,7 +0,0 @@
-C:\fengdev\github\centolla-tools\mke\target\debug\deps\mke.exe: src/main.rs src\argsgrappler\mod.rs src\argsgrappler\functions.rs
-
-C:\fengdev\github\centolla-tools\mke\target\debug\deps\mke.d: src/main.rs src\argsgrappler\mod.rs src\argsgrappler\functions.rs
-
-src/main.rs:
-src\argsgrappler\mod.rs:
-src\argsgrappler\functions.rs:
--- mke/target/debug/mke.d
+++
@@ -1,1 +0,0 @@
-C:\fengdev\github\centolla-tools\mke\target\debug\mke.exe: C:\fengdev\github\centolla-tools\mke\src\argsgrappler\functions.rs C:\fengdev\github\centolla-tools\mke\src\argsgrappler\mod.rs C:\fengdev\github\centolla-tools\mke\src\main.rs

8b42a3f6057ea5c2058f1b90715f4a5ca736ce8b
feng <feng@vitruvio.cl>
2024-11-11 12:48:58 -0300

    created

---
+++ mke/target/debug/deps/mke-26cc617318d9cb35.d
@@ -0,0 +1,5 @@
+C:\fengdev\github\centolla-tools\mke\target\debug\deps\libmke-26cc617318d9cb35.rmeta: src/main.rs
+
+C:\fengdev\github\centolla-tools\mke\target\debug\deps\mke-26cc617318d9cb35.d: src/main.rs
+
+src/main.rs:
---
+++ mke/target/debug/deps/mke-c0f7a78663fc35bc.d
@@ -0,0 +1,5 @@
+C:\fengdev\github\centolla-tools\mke\target\debug\deps\libmke-c0f7a78663fc35bc.rmeta: src/main.rs
+
+C:\fengdev\github\centolla-tools\mke\target\debug\deps\mke-c0f7a78663fc35bc.d: src/main.rs
+
+src/main.rs:

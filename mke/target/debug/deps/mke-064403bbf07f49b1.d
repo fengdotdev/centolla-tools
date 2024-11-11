e95e99951e7a1cabc0d3127942ef31d0cfa735d1
feng.dev <feng@vitruvio.cl>
2024-11-11 19:03:09 -0300

    ...

---
+++ mke/target/debug/deps/mke-064403bbf07f49b1.d
@@ -0,0 +1,10 @@
+/Users/feng/Documents/GitHub/centolla-tools/mke/target/debug/deps/libmke-064403bbf07f49b1.rmeta: src/main.rs src/argsgrappler/mod.rs src/argsgrappler/functions.rs src/models/mod.rs src/models/makefile_model.rs src/models/task_model.rs
+
+/Users/feng/Documents/GitHub/centolla-tools/mke/target/debug/deps/mke-064403bbf07f49b1.d: src/main.rs src/argsgrappler/mod.rs src/argsgrappler/functions.rs src/models/mod.rs src/models/makefile_model.rs src/models/task_model.rs
+
+src/main.rs:
+src/argsgrappler/mod.rs:
+src/argsgrappler/functions.rs:
+src/models/mod.rs:
+src/models/makefile_model.rs:
+src/models/task_model.rs:
---
+++ mke/target/debug/deps/mke-fb3d88c0d464dfc3.d
@@ -0,0 +1,10 @@
+/Users/feng/Documents/GitHub/centolla-tools/mke/target/debug/deps/libmke-fb3d88c0d464dfc3.rmeta: src/main.rs src/argsgrappler/mod.rs src/argsgrappler/functions.rs src/models/mod.rs src/models/makefile_model.rs src/models/task_model.rs
+
+/Users/feng/Documents/GitHub/centolla-tools/mke/target/debug/deps/mke-fb3d88c0d464dfc3.d: src/main.rs src/argsgrappler/mod.rs src/argsgrappler/functions.rs src/models/mod.rs src/models/makefile_model.rs src/models/task_model.rs
+
+src/main.rs:
+src/argsgrappler/mod.rs:
+src/argsgrappler/functions.rs:
+src/models/mod.rs:
+src/models/makefile_model.rs:
+src/models/task_model.rs:
